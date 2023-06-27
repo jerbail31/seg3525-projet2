fbe994b4db3d152860e43aae24dd6496192d6882
Jérémy Baillargeon <jerbail31@gmail.com>
2023-06-27 11:54:12 -0400

    update

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 
 const CustomButton = ({ id, title, onClick, includes, price }) => {
+  var includeObject = [];
+  if (includes[0] === true) {
+    includeObject.push('Grass');
+  }
+  if (includes[1] === true) {
+    includeObject.push('Trimming');
+  }
+  if (includes[2] === true) {
+    includeObject.push('Leaf removal');
+  }
+  if (includes[3] === true) {
+    includeObject.push('Aeration');
+  }
 
-  price = 'Price :' + price;
+
+  price = 'Price: ' + price;
   return (
-    <div id={id} className="services-card">
-      <h4>{ title }</h4>
-      <h5>Service includes</h5>
-      <div>
-        { includes }
-      </div>
-      <h5>{ price } </h5>
-      <Button onClick={ onClick }> Book </Button>
+    <div id={id} className="serviceCard-main">
+      <Card className='serviceCard-card'>
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <div className='serviceCard-includes'>
+            <Card.Subtitle>Service includes:</Card.Subtitle>
+            <ul>
+              {includeObject.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
+          <Card.Subtitle className='serviceCard-price'>{price} </Card.Subtitle>
+          <Button variant='success' className='serviceCard-button' onClick={onClick}> Book </Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Form } from 'react-bootstrap';
+import { Container, Row, Col, Form, Card } from 'react-bootstrap';
 import React, { useState, useEffect } from 'react';
 import ServiceCard from './ServiceCard.js';
 import { useNavigate } from 'react-router-dom';
@@ -51,7 +51,6 @@ function Services() {
 
   const serviceList = [yearGrass, yearTrim, yearFull];
   useEffect(() => {
-    debugger;
     filterCards();
   });
 
@@ -60,10 +59,10 @@ function Services() {
       if (noFilters()) {
         document.getElementById(serviceList[i][0]).style.display = 'block';
       }
-      else if (filterService(serviceList[i])) { 
+      else if (filterService(serviceList[i])) {
         document.getElementById(serviceList[i][0]).style.display = 'block';
       }
-      else{
+      else {
         document.getElementById(serviceList[i][0]).style.display = 'none';
       }
     }
@@ -72,27 +71,27 @@ function Services() {
     return (!(isCheckedGrass || isCheckedTrim || IsCheckedAeration || IsCheckedLeaf || !(selectedOccurence === '')));
   }
   const filterService = (service) => {
-    if (!(selectedOccurence === '' || selectedOccurence === service[1])){
+    if (!(selectedOccurence === '' || selectedOccurence === service[1])) {
       return false;
     }
-    else if(isCheckedGrass || isCheckedTrim || IsCheckedAeration || IsCheckedLeaf){
-      if(isCheckedGrass){
-        if(service[2][0] === false){
+    else if (isCheckedGrass || isCheckedTrim || IsCheckedAeration || IsCheckedLeaf) {
+      if (isCheckedGrass) {
+        if (service[2][0] === false) {
           return false
         }
       }
-      if(isCheckedTrim){
-        if(service[2][1] === false){
+      if (isCheckedTrim) {
+        if (service[2][1] === false) {
           return false
         }
       }
-      if(IsCheckedAeration){
-        if(service[2][2] === false){
+      if (IsCheckedAeration) {
+        if (service[2][2] === false) {
           return false
         }
       }
-      if(IsCheckedLeaf){
-        if(service[2][3] === false){
+      if (IsCheckedLeaf) {
+        if (service[2][3] === false) {
           return false
         }
       }
@@ -105,80 +104,90 @@ function Services() {
       <Container>
         <Row>
           <Col md={3}>
-            <div className='services-filter'>
+            <Card className='services-filter'>
+              <Card.Body>
+                <Form>
+                  <Card.Title>Filters</Card.Title>
+                  <div className='services-filterGroup'>
+                    <Card.Subtitle>Occurence</Card.Subtitle>
+                    <div className='services-filterItems'>
+                      <Form.Check
+                        type="radio"
+                        id="radioseasonal"
+                        label="Seasonal"
+                        value="seasonal"
+                        checked={selectedOccurence === 'seasonal'}
+                        onChange={handleRadioChange}
+                      />
 
-              <Form>
-                <h4>Filters</h4>
-                <h5>Occurence</h5>
-                <Form.Check
-                  type="radio"
-                  id="radioseasonal"
-                  label="Seasonal"
-                  value="seasonal"
-                  checked={selectedOccurence === 'seasonal'}
-                  onChange={handleRadioChange}
-                />
+                      <Form.Check
+                        type="radio"
+                        id="radioSingle"
+                        label="Single"
+                        value="single"
+                        checked={selectedOccurence === 'single'}
+                        onChange={handleRadioChange}
+                      />
 
-                <Form.Check
-                  type="radio"
-                  id="radioSingle"
-                  label="Single"
-                  value="single"
-                  checked={selectedOccurence === 'single'}
-                  onChange={handleRadioChange}
-                />
+                      <Form.Check
+                        type="radio"
+                        id="radioAddon"
+                        label="Add-ons"
+                        value="addon"
+                        checked={selectedOccurence === 'addon'}
+                        onChange={handleRadioChange}
+                      />
+                    </div>
+                  </div>
+                  <div className='services-filterGroup'>
+                    <Card.Subtitle>Service Type</Card.Subtitle>
+                    <div className='services-filterItems'>
+                      <Form.Check
+                        type="checkbox"
+                        id="checkboxGrass"
+                        label="Grass"
+                        checked={isCheckedGrass}
+                        onChange={handleCheckboxChangeGrass}
+                      />
 
-                <Form.Check
-                  type="radio"
-                  id="radioAddon"
-                  label="Add-ons"
-                  value="addon"
-                  checked={selectedOccurence === 'addon'}
-                  onChange={handleRadioChange}
-                />
-                <h5>Service Type</h5>
-                <Form.Check
-                  type="checkbox"
-                  id="checkboxGrass"
-                  label="Grass"
-                  checked={isCheckedGrass}
-                  onChange={handleCheckboxChangeGrass}
-                />
+                      <Form.Check
+                        type="checkbox"
+                        id="checkboxTrim"
+                        label="Trimming"
+                        checked={isCheckedTrim}
+                        onChange={handleCheckboxChangeTrim}
+                      />
 
-                <Form.Check
-                  type="checkbox"
-                  id="checkboxTrim"
-                  label="Trimming"
-                  checked={isCheckedTrim}
-                  onChange={handleCheckboxChangeTrim}
-                />
+                      <Form.Check
+                        type="checkbox"
+                        id="checkboxAeration"
+                        label="Aeration"
+                        checked={IsCheckedAeration}
+                        onChange={handleCheckboxChangeAeration}
+                      />
+                      <Form.Check
+                        type="checkbox"
+                        id="checkboxLeaf"
+                        label="Leaf Removal"
+                        checked={IsCheckedLeaf}
+                        onChange={handleCheckboxChangeLeaf}
+                      />
+                    </div>
+                  </div>
 
-                <Form.Check
-                  type="checkbox"
-                  id="checkboxAeration"
-                  label="Aeration"
-                  checked={IsCheckedAeration}
-                  onChange={handleCheckboxChangeAeration}
-                />
-                <Form.Check
-                  type="checkbox"
-                  id="checkboxLeaf"
-                  label="Leaf Removal"
-                  checked={IsCheckedLeaf}
-                  onChange={handleCheckboxChangeLeaf}
-                />
-                <p>
-                  Prices vary according to yard size. An estimate will be determined during the booking phase.
-                </p>
-              </Form>
-            </div>
+                  <Card.Text className='services-filterText'>
+                    Prices vary according to yard size. An estimate will be determined during the booking phase.
+                  </Card.Text>
+                </Form>
+              </Card.Body>
+            </Card>
           </Col>
           <Col md={9}>
             <Container id='cardContainer' className='services-cardContainer'>
 
-              <ServiceCard id="cardYearGrass" title='Basic Seasonal Package' onClick={book} price='50$' includes='nothing'></ServiceCard>
-              <ServiceCard id="cardYearTrim" title='Advanced Seasonal Package' onClick={book} price='50$' includes='nothing'></ServiceCard>
-              <ServiceCard id="cardYearFull" title='Super Seasonal Package' onClick={book} price='50$' includes='nothing'></ServiceCard>
+              <ServiceCard id="cardYearGrass" title='Basic Seasonal Package' onClick={book} price='50$' includes={yearGrass[2]}></ServiceCard>
+              <ServiceCard id="cardYearTrim" title='Advanced Seasonal Package' onClick={book} price='50$' includes={yearTrim[2]}></ServiceCard>
+              <ServiceCard id="cardYearFull" title='Super Seasonal Package' onClick={book} price='50$' includes={yearFull[2]}></ServiceCard>
 
 
             </Container>
